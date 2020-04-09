d637cc604778dea06a61adf4b35bdd16637a0114
Gagan <gagankaushal@users.noreply.github.com>
2020-04-09 11:16:04 -0400

    Added webpage redirection

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-var http = require("http"),
-    exec = require("child_process").exec;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-
-  //executes my shell script - main.sh when a request is posted to the server
-  exec('sh server/build/distributions/server-1.0/bin/server', function (err, stdout, stderr) {
-    if (err) handleError();
-
-    //Print stdout/stderr to console
-    console.log(stdout);
-    console.log(stderr);
-
-    //Simple response to user whenever localhost:8888 is accessed
-    response.write(stdout);
-    response.end();
-  });
-}
-
-http.createServer(onRequest).listen(8888, function () {
-  console.log("Server has started.");
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, ''))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
