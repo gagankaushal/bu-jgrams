0c0fbdf5806e71fcd7b6c0169c47ddb102777566
Gagan <gagankaushal@users.noreply.github.com>
2020-04-09 00:59:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require("http"),
+    exec = require("child_process").exec;
+
+function onRequest(request, response) {
+  console.log("Request received.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  //executes my shell script - main.sh when a request is posted to the server
+  exec('server/build/distributions/server-1.0/bin/server', function (err, stdout, stderr) {
+    if (err) handleError();
+
+    //Print stdout/stderr to console
+    console.log(stdout);
+    console.log(stderr);
+
+    //Simple response to user whenever localhost:8888 is accessed
+    response.write(stdout);
+    response.end();
+  });
+}
+
+http.createServer(onRequest).listen(8888, function () {
+  console.log("Server has started.");
+});
