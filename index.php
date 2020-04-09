b79119c50d2328f28511fd8e7a99f261f06b942f
Gagan <gagankaushal@users.noreply.github.com>
2020-04-09 11:36:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec("server\build\distributions\server-1.0\bin\server");
+$output = shell_exec("server\build\distributions\server-1.0\bin\server1");
 echo "<pre>$output</pre>";
 ?>
